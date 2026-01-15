abf10b22c0d4b8bc30c070b3a82e012e5483fb95
wzagouri4457 <w.zagouri4457@uca.ac.ma>
2026-01-15 10:41:04 +0100

    Add basic arithmetic operations in arithmétique.py

---
+++ arithmétique.py
@@ -0,0 +1,9 @@
+a = 15
+b = 4
+print(f"{a} + {b} = {a + b}")
+print(f"{a} - {b} = {a - b}")
+print(f"{a} * {b} = {a * b}")
+print(f"{a} / {b} = {a / b}")
+print(f"{a} // {b} = {a // b}")    
+print(f"{a} % {b} = {a % b}")      
+print(f"{a} ** {b} = {a ** b}")  
